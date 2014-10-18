304d82fffe94eb8d80bec130ebb0270aeb597aea
Khang Huynh <khang.huynh@gmail.com>
2014-10-18 14:46:12 -0400

    Initial Commit

---
+++ TaskIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskIt
+//
+//  Created by Khang Huynh on 2014-10-18.
+//  Copyright (c) 2014 Khang Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
