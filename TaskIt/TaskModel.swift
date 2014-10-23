01b96c66164849601012e0bd57632fefb44f6a35
Khang Huynh <khang.huynh@gmail.com>
2014-10-22 21:42:00 -0400

    Added TaskModel, refactored tableView code

---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,15 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by Khang Huynh on 2014-10-22.
+//  Copyright (c) 2014 Khang Huynh. All rights reserved.
+//
+
+import Foundation
+
+struct TaskModel {
+    var task:String
+    var subTask:String
+    var date:String
+}
