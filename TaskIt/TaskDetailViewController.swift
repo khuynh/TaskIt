4eee01835318627edc0ec09b8614eec6972a5ebe
Khang Huynh <khang.huynh@gmail.com>
2014-10-25 18:52:08 -0400

    Added Date class and date functions

--- TaskIt/TaskDetailViewController.swift
+++ TaskIt/TaskDetailViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class TaskDetailViewController: UIViewController {
 
-    var detailTaskModel:TaskModel!
+    var detailTaskModel: TaskModel!
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var subtaskTextField: UITextField!
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.taskTextField.text = detailTaskModel.task
+        self.subtaskTextField.text = detailTaskModel.subTask
+        self.dueDatePicker.date = detailTaskModel.date
+        
     }
 
     override func didReceiveMemoryWarning() {
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -18,10 +18,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: "01/14/2014")
-        let task2 = TaskModel(task: "Eat Dinner", subTask: "Burgers", date: "01/14/2014")
+        let date1 = Date.from(year: 2014, month: 05, day: 20)
+        let date2 = Date.from(year: 2014, month: 03, day: 3)
+        let date3 = Date.from(year: 2014, month: 12, day: 13)
         
-        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Leg day", date: "01/14/2014")]
+        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: date1)
+        let task2 = TaskModel(task: "Eat Dinner", subTask: "Burgers", date: date2)
+        
+        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Leg day", date: date3)]
         
         self.tableView.reloadData()
     }
@@ -34,13 +38,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         if segue.identifier == "showTaskDetail" {
-            let detailVC:TaskDetailViewController = segue.destinationViewController as TaskDetailViewController
-            
-            //let indexPath = self.tableView.indexPathForSelectedRow()
-            
-            //let thisTask = taskArray[indexPath!.row]
-            
-            //detailVC.detailTaskModel = thisTask
+            let detailVC: TaskDetailViewController = segue.destinationViewController as TaskDetailViewController
+            let indexPath = self.tableView.indexPathForSelectedRow()
+            let thisTask = taskArray[indexPath!.row]
+            detailVC.detailTaskModel = thisTask
             
         }
         
@@ -60,7 +61,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         cell.taskLabel.text = thisTask.task
         cell.descriptionLabel.text = thisTask.subTask
-        cell.dateLabel.text = thisTask.date
+        cell.dateLabel.text = Date.toString(date: thisTask.date)
         
         return cell
     }
