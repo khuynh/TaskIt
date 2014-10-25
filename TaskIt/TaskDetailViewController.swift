45a94b910814b1489c651c75b5f066c709cea579
Khang Huynh <khang.huynh@gmail.com>
2014-10-25 17:32:15 -0400

    Added TaskDetailViewController, segue code

---
+++ TaskIt/TaskDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TaskDetailViewController.swift
+//  TaskIt
+//
+//  Created by Khang Huynh on 2014-10-25.
+//  Copyright (c) 2014 Khang Huynh. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    var detailTaskModel:TaskModel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -30,6 +30,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if segue.identifier == "showTaskDetail" {
+            let detailVC:TaskDetailViewController = segue.destinationViewController as TaskDetailViewController
+            
+            //let indexPath = self.tableView.indexPathForSelectedRow()
+            
+            //let thisTask = taskArray[indexPath!.row]
+            
+            //detailVC.detailTaskModel = thisTask
+            
+        }
+        
+    }
 
     // UITableViewDataSource
     
@@ -53,7 +68,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     // UITableViewDelegate
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
+        println(indexPath.row)
+        performSegueWithIdentifier("showTaskDetail", sender: self)
     }
     
 }
